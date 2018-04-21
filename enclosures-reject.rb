b640cf026f4a3ac8f5196bd67756038d1b87ba09
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2018-04-21 14:22:30 +0300

    reduce mk.root usage

--- enclosures-reject.rb
+++ enclosures-reject.rb
@@ -3,5 +3,5 @@ require 'pstore'
 # input: media/foo.m4a!http://example.com/file.m4a!.mp3
 while (line = gets)
   _name, url, _ext = line.split '!'
-  puts line if `ruby #{ENV['mk.root']}/history.rb get "#{url}"` == ""
+  puts line if `ruby #{__dir__}/history.rb get "#{url}"` == ""
 end
