17f1ed36fbddc8d4dc19d3ab0a15429eee56f73e
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2018-04-21 17:37:09 +0300

    inline enclosures-reject

--- enclosures-reject.rb
+++
@@ -1,7 +0,0 @@
-require 'pstore'
-
-# input: media/foo.m4a!http://example.com/file.m4a!.mp3
-while (line = gets)
-  _name, url, _ext = line.split '!'
-  puts line if `ruby #{__dir__}/history.rb get "#{url}"` == ""
-end
