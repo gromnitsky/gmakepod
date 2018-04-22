334b54921e9f7f7d665e2378a5401fa346ffdf5b
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2018-04-22 12:03:16 +0300

    unescape urls

--- enclosures-print.rb
+++ enclosures-print.rb
@@ -1,5 +1,6 @@
 require 'date'
 require 'uri'
+require 'cgi'
 
 def simple p; File.basename p; end
 def timestamp p
@@ -7,11 +8,12 @@ def timestamp p
 end
 
 ofile = ENV['t'].to_s != '' ? method(:timestamp) : method(:simple)
+def normalize p; CGI.unescape(p).gsub(/[\s%!]/, "_"); end
 
 # input: foo!http://example.com/file.m4v!.mp3
 # output: media/foo/file.m4v!http://example.com/file.m4v!.mp3
 while (line = gets)
   name, feed, convert_to, reverse = line.split("!")
-  p = URI(feed).path
+  p = normalize URI(feed).path
   puts [File.join('media', name, ofile.call(p)), feed, convert_to, reverse].join '!'
 end
