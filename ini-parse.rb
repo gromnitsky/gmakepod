23219e84bd6cdd673d4a6fe932c7c5f6eb715d59
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2018-04-22 05:52:27 +0300

    fix s= opt

--- ini-parse.rb
+++ ini-parse.rb
@@ -2,5 +2,5 @@ require 'inifile'
 
 fail "Usage: #{$0} file.ini" unless ARGV[0]
 
-puts IniFile.new(content: `erb "#{ARGV[0]}"`).to_h
+puts IniFile.new(content: `erb "#{File.realpath ARGV[0]}"`).to_h
        .map{|k,v| [k, v["url"], v["convert-to"], v["reverse"]].join "!"}
