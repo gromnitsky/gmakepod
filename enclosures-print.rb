3fda308bb3550a153c4803dce236cf22755460e8
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2018-04-25 14:51:17 +0300

    rm t= opt

--- enclosures-print.rb
+++ enclosures-print.rb
@@ -4,15 +4,13 @@ require_relative 'u'
 
 def parse line; line[1..-1].split('!').map{|exp| exp.split(':=', 2)}.to_h; end
 
-def simple p; File.basename p; end
 def timestamp p
   File.basename(p, ".*") + "." + DateTime.now.strftime("%Q") + File.extname(p)
 end
 
-ofile = ENV['t'].to_s != '' ? method(:timestamp) : method(:simple)
-
 while (line = gets)
   c = parse line
-  c['.name'] = File.join('media', c['.name'], ofile.call(norm URI(c['.url']).path))
+  p = URI(c['.url']).path; p = 'noname.noext' if p.size == 0
+  c['.name'] = File.join('media', c['.name'], timestamp(norm p))
   puts ':' + c.map{|k,v| "#{k}:=#{v}"}.join('!')
 end
