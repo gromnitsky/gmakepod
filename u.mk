bbac2b6a333e8662fba6b6b01cfa3480feb9084b
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2018-04-26 17:36:36 +0300

    fix parser initialization

--- u.mk
+++ u.mk
@@ -4,5 +4,5 @@ define newline
 endef
 
 conf_parse = $(eval $(subst !,$(newline),$(shell echo '$1' | awk '{print substr($$0, 2)}')))
-conf_parse_init = $(call conf_parse,:.name=!.url=!.reverse=!.e=!.convert-to=)
+conf_parse_init = $(call conf_parse,:.name=!.url=!.reverse=!.e=!.convert-to=!.filter.name=!.filter.url=)
 eq = $(and $(findstring x$(1),x$(2)), $(findstring x$(2),x$(1)))
