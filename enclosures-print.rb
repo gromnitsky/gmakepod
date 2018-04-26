3316073bfb73798d607a48ee00126ae101ec6073
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2018-04-26 10:16:03 +0300

    use securerandom in datetime stead

--- enclosures-print.rb
+++ enclosures-print.rb
@@ -1,16 +1,16 @@
-require 'date'
+require 'securerandom'
 require 'uri'
 require_relative 'u'
 
 def parse line; line[1..-1].split('!').map{|exp| exp.split(':=', 2)}.to_h; end
 
-def timestamp p
-  File.basename(p, ".*") + "." + DateTime.now.strftime("%Q") + File.extname(p)
+def filename p
+  File.basename(p, ".*") + "." + SecureRandom.hex(4) + File.extname(p)
 end
 
 while (line = gets)
   c = parse line
   p = URI(c['.url']).path; p = 'noname.noext' if p.size == 0
-  c['.name'] = File.join('media', c['.name'], timestamp(norm p))
+  c['.name'] = File.join('media', c['.name'], filename(norm p))
   puts ':' + c.map{|k,v| "#{k}:=#{v}"}.join('!')
 end
