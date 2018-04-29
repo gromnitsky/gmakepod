37188a916ba2961a2f4307a68eb0896ba97c552d
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2018-04-29 12:00:13 +0300

    inline json2make.rb

--- json2make.rb
+++
@@ -1,4 +0,0 @@
-require 'json'
-
-separator = '9dcd4654-4b01-11e8-9491-000c2945132f'
-puts JSON.parse(ARGV[0][1..-1]).map{|k,v| "#{k}:=#{v}"}.join(separator)
--- u.mk
+++ u.mk
@@ -8,7 +8,7 @@ comma := ,
 
 se = '$(subst ','\'',$(1))'
 #'# emacs font-lock
-conf_parse = $(eval $(subst $(separator),$(newline),$(shell ruby $(src)/json2make.rb $(call se,$1))))
+conf_parse = $(eval $(subst $(separator),$(newline),$(shell echo $(call se,$1) | ruby -rjson -n -e 'puts JSON.parse($$_[1..-1]).map{|k,v| "\#{k}:=\#{v}"}.join("$(separator)")' )))
 conf_parse_init = $(call conf_parse,:{".name": ""$(comma) ".url": ""$(comma) ".reverse": ""$(comma) ".e": ""$(comma) ".convert-to": ""$(comma) ".filter.name": ""$(comma) ".filter.url": ""})
 eq = $(and $(findstring x$(1),x$(2)), $(findstring x$(2),x$(1)))
 echo = $(if $(findstring s,$(firstword -$(MAKEFLAGS))),,@echo $1 1>&2)
