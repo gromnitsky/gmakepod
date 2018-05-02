15122255fba2efa5734297aa9b6e2c6080bf0ce8
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2018-05-02 15:25:11 +0300

    speed-up enclosures-reject.mk

--- enclosures-reject.mk
+++ enclosures-reject.mk
@@ -2,4 +2,4 @@ src := $(dir $(realpath $(lastword $(MAKEFILE_LIST))))
 include $(src)/u.mk
 %:
 	@$(call props_parse,$*)
-	-@ruby $(src)/history.rb get $(call se,$(.url)) > /dev/null || echo $(call se,$(call se,$*))
+	-@ruby --disable-gems $(src)/history.rb get $(call se,$(.url)) > /dev/null || echo $(call se,$(call se,$*))
